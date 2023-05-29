8c01e4dce18163c3a089de924c9671665b135b55
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-29 19:11:38 +0530

    Probable change the Nav later

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./components/Home";
-import Navbar from "./components/Navigation";
+import Home from "./pages/Home";
+import Navigation from "./components/Navigation";
 import Services from "./components/Services";
 import Product from "./components/Product";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/navigation" element={<Navigation />} />
       </Routes>
     </>
   );
