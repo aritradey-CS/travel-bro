29e84a061eb63b99c3d60bf67d8dff2db1611bac
aritradey-CS <81703791+aritradey-CS@users.noreply.github.com>
2023-05-31 21:41:48 +0530

    adding something new2

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CardItem from './CardItem';
-import './Cards.css';
+import './Card.css';
 
 
 function Cards() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '..//App.css';
-// import Cards from '../Cards';
+import Cards from '../pages/Cards.js';
 import HeroSection from '../pages/HeroSection';
 // import Footer from '../Footer';
 
@@ -8,7 +8,8 @@ function Home() {
   return (
     <>
       <HeroSection />
-      {/* <Cards /> */}
+      <Cards />
+      
       {/* <Footer /> */}
     </>
   );
